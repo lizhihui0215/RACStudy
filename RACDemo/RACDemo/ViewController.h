477d14a207d625a540ef1d230b1331174a3245d5
李智慧 <lizhihui@richikeis-MacBook-Pro.local>
2015-08-19 21:39:18 +0800

    add project and change .gitignore

---
+++ RACDemo/RACDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RACDemo
+//
+//  Created by 李智慧 on 8/19/15.
+//  Copyright © 2015 lizhihui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RACDemo/RACDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RACDemo
+//
+//  Created by 李智慧 on 8/19/15.
+//  Copyright © 2015 lizhihui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
